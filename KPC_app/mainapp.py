ec061000f58f1971f30163c942a10b87f397a906
takuaki <tatennisku@gmail.com>
2024-09-04 02:26:11 +0900

    image edit

--- KPC_app/mainapp.py
+++ KPC_app/mainapp.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+import pandas as pd
 
 st.title('Kyotanabe Pickleball Club')
 st.caption('これは京田辺ピックルボールクラブメンバーアプリです。')
@@ -10,7 +11,9 @@ st.text('This is the app for member of Kyotanabe Pickleball Club. It is just for
 
 
 #画像
-image_path = "./Data/picklepicture.jpeg"
-image = Image.open('image_path')
+image = Image.open('./Data/picklepicture.jpeg')
 st.image(image, width=200)
 
+#データ分析関連
+df = pd.read_csv('./Data/averagetemp.csv', index_col='month')
+st.line_chart(df)
