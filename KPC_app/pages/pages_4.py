24a097214f2ed956fed614fc253b18128067f4dc
takuaki <tatennisku@gmail.com>
2024-09-03 12:04:52 +0900

    add profile html file

--- KPC_app/pages/pages_4.py
+++ KPC_app/pages/pages_4.py
@@ -3,3 +3,13 @@ import streamlit as st
 #画像
 image = Image.open('./data/picklepicture.jpeg')
 st.image(image, width=200)
+
+code = '''
+
+import streamlit as st
+
+st.title('Kyotanabe Pickleball Club Member app')
+
+'''
+
+st.code(code,language='python')
