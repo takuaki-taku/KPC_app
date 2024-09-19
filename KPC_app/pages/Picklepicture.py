f78fff37f87ae8670d87665db9361c37c149ac19
takuaki-taku <tatennisku@gmail.com>
2024-09-19 07:22:57 +0000

    aaaaaa

--- KPC_app/pages/Picklepicture.py
+++ KPC_app/pages/Picklepicture.py
@@ -4,3 +4,4 @@ from PIL import Image
 #画像
 image = Image.open('KPC_app/Data/picklepicture.jpeg')
 st.image(image, width=200)
+
--- KPC_app/pages/Video.py
+++ KPC_app/pages/Video.py
@@ -5,3 +5,4 @@ video_file = open('KPC_app/Data/picklevideo1.mp4','rb')
 video_bytes = video_file.read()
 st.video(video_bytes)
 
+
