fb7454f7cfe09189e31319e4fde70c5e69ab2cf2
takuaki-taku <tatennisku@gmail.com>
2024-09-19 06:32:17 +0000

    fromcodespace

--- KPC_app/pages/pages_3.py
+++ KPC_app/pages/pages_3.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 #動画
-video_file = open('./Data/picklevideo1.mp4','rb')
+video_file = open('KPC_app/Data/picklevideo1.mp4','rb')
 video_bytes = video_file.read()
 st.video(video_bytes)
 
